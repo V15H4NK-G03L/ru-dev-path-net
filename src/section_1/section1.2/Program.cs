4c9bac478b3755f3f42de08e40fc23007bee8215
Vishank Goel (GitK) <vishankgoel11@gmail.com>
2026-08-29 06:08:46 +0530

    unit one

--- src/section_1/section1.2/Program.cs
+++ src/section_1/section1.2/Program.cs
@@ -1,5 +1,22 @@
 ﻿using StackExchange.Redis;
 
 // TODO for Coding Challenge Start here on starting-point branch
+var muxer = ConnectionMultiplexer.Connect("localhost:6379");
+IDatabase db = muxer.GetDatabase();
+
+Console.WriteLine();
+Console.WriteLine(db.Ping());
+
+string? ans = db.StringGet("test");
+Console.WriteLine(ans);
+
+db.StringSet("dotnet","dotnet_testing");
+ans = db.StringGet("dotnet");
+Console.WriteLine(ans);
+
+ans = db.StringGet("hi");
+Console.WriteLine(ans);
+ans = db.StringGet("12");
+Console.WriteLine(ans);
 
 // end programming challenge
